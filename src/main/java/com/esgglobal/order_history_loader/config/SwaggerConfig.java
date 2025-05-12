1326c4e04819d348054151cecd6147bcace36132
mefendja.chinedu <mefendja.chinedu@azenia.com>
2025-05-11 22:29:16 -0600

    Resolved issue with swagger

--- src/main/java/com/esgglobal/order_history_loader/config/SwaggerConfig.java
+++ src/main/java/com/esgglobal/order_history_loader/config/SwaggerConfig.java
@@ -1,16 +1,30 @@
 package com.esgglobal.order_history_loader.config;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
-import io.swagger.v3.oas.annotations.info.Info;
+//import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.OpenAPI;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
 
 @Configuration
-@OpenAPIDefinition(
-    info = @Info(
-        title = "Order Service API",
-        version = "1.0",
-        description = "API documentation for managing orders"
-    )
-)
 public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Order Service API")
+                        .version("1.0")
+                        .description("API documentation for managing orders"));
+    }
+
+    @Bean
+    public Docket productApi() {
+        return new Docket(DocumentationType.SWAGGER_2).select()
+                .apis(RequestHandlerSelectors.basePackage("com/esgglobal/order_history_loader/controller")).build();
+    }
 }
